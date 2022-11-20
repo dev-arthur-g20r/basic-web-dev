c095ba8e6ddfe2813576d0ecd6263859eaa08291
Arthur Tristan M. Ramos <52824403+dev-arthur-g20r@users.noreply.github.com>
2022-11-20 18:13:30 +0800

    Document some needed facts in forums.js by commenting

--- Front End/webpage/js/main/forums.js
+++ Front End/webpage/js/main/forums.js
@@ -5,6 +5,7 @@ https://jquery.com/download/
 To create jquery.min.js, you can copy contents from CDN then save it as your own jquery.min.js file.
 */
 
+// Convert these characters to HTML escaping characters.
 function cleanString(value) {
 	return value.replace(/&/g, "&amp;")
 		.replace(/>/g, "&gt;")
@@ -16,9 +17,19 @@ function cleanString(value) {
 function displayForums() {
 	var url = "http://localhost/pythomyapi/forums.php"
 	$.getJSON(url, function(data) {
-		$("#forums").empty();
-		if (data != null && 
-			data.payload != null) {
+		/*
+			$("#forums")
+
+			is shorthand equivalent for:
+
+			document.getElementById("forums")
+		*/
+
+		// Clear list of forums on call of function since this is mostly used for reloading data.
+		$("#forums").empty(); 
+
+		// `data` automatically returns responseJSON.
+		if (data != null && data.payload != null) {
 			if (data.payload.length > 0) {
 				var forumsList = data.payload
 				for (var index = 0; index < forumsList.length; index++) {
@@ -31,13 +42,17 @@ function displayForums() {
 					"<p>" + cleanString(forumData.body) +"</p>" +
 					"<div class='divider'></div>" +
 					"</li>"
+					/*
+						<strong> == <b>
+						<em> == <i>
+					*/
 					$("#forums").append(forumItem)
 				}
 			}
 		}
 	}).fail(function(error){
 		if (error.responseJSON != null) {
-			var errorJSON = error.responseJSON
+			var errorJSON = error.responseJSON // Need to call responseJSON property of `error`.
 			var statusCode = parseInt(errorJSON.statusCode)
 			if (statusCode == 400) {
 				window.alert("You have sent a bad request.")
@@ -50,10 +65,12 @@ function postForum() {
 	var url = "http://localhost/pythomyapi/addforum.php"
 	var title = $("#fld_title").val().trim()
 	var body = $("#fld_body").val().trim()
+
 	var data = JSON.stringify({
 		title: title,
 		body: body
-	})
+	}) // Convert to JSON string.
+
 	$.post(url, data, function(info) {
 		if (info != null && info.statusCode != null) {
 			var statusCode = parseInt(info.statusCode)
@@ -62,17 +79,17 @@ function postForum() {
 			} else {
 				window.alert("Failed to post forum! Please try again.")
 			}
-			displayForums()
+			displayForums() // Update list of forums.
 		}
 	}).fail(function(error) {
 		if (error.responseJSON != null) {
 			var errorJSON = error.responseJSON
 			if (errorJSON.statusCode != null && errorJSON.statusCode == 403) {
 				window.alert("Failed to post forum! Please try again.")
-				displayForums()
+				displayForums() // Update list of forums.
 			}
 		}
 	})
 }
 
-displayForums()
+displayForums() // Display forums on load of page.
